614c8323220d28989bf39f5faf872e884127d25e
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2023-10-08 14:50:18 -0300

    Designing the 'small-card' component

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
-export class SmallCardComponent {
-
-}
+export class SmallCardComponent {}
