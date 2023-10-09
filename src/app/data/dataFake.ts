db3f4dc2a9b7e032e7a1ca2aeb86e6bd5e58aa4e
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2023-10-09 16:12:39 -0300

    Accessing page data dynamically

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -13,4 +13,18 @@ export const dataFake = [
     photoCover:
       'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg',
   },
+  {
+    id: '3',
+    title: 'Novo filme do pantera negra lançado em breve',
+    description: 'bla bla bla',
+    photoCover:
+      'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg',
+  },
+  {
+    id: '4',
+    title: 'Novo filme dos X-men está em pós produção, afirma diretor',
+    description: 'bla bla bla',
+    photoCover:
+      'https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339',
+  },
 ];
