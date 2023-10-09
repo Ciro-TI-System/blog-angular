6b5716579447bfef7d5163bae52b073baf533942
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2023-10-09 13:14:53 -0300

    Creation of 'page/content' and routes
    Decoupling components from the visual part, creating and styling other pages and adding their routes.

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.css'],
 })
+export class MenuBarComponent implements OnInit {
+  constructor() {}
 
-export class MenuBarComponent {}
+  ngOnInit(): void {}
+}
